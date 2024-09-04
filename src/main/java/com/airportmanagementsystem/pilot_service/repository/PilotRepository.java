645c63bf7813780324e7b3c008bac163564a0d3b
Tharusha Dilhara <egodagahagedaratharusha2000@gmail.com>
2024-09-05 01:17:09 +0530

    Updated

--- src/main/java/com/airportmanagementsystem/pilot_service/repository/PilotRepository.java
+++ src/main/java/com/airportmanagementsystem/pilot_service/repository/PilotRepository.java
@@ -1,10 +1,10 @@
-package com.airportmanagementsystem.pilot_service.repo;
+package com.airportmanagementsystem.pilot_service.repository;
 
 import com.airportmanagementsystem.pilot_service.entity.Pilot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface PilotRepo extends JpaRepository<Pilot, Integer> { //<entity name, PK data type
+public interface PilotRepository extends JpaRepository<Pilot, Integer> { //<entity name, PK data type
 
     // For Native Query // pilot's details filter by using pilot_ID
     @Query(value = "SELECT * FROM pilot WHERE pilot_id = ?1",nativeQuery = true)
--- src/main/java/com/airportmanagementsystem/pilot_service/service/PilotService.java
+++ src/main/java/com/airportmanagementsystem/pilot_service/service/PilotService.java
@@ -2,7 +2,7 @@ package com.airportmanagementsystem.pilot_service.service;
 
 import com.airportmanagementsystem.pilot_service.dto.PilotDTO;
 import com.airportmanagementsystem.pilot_service.entity.Pilot;
-import com.airportmanagementsystem.pilot_service.repo.PilotRepo;
+import com.airportmanagementsystem.pilot_service.repository.PilotRepository;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -15,25 +15,25 @@ import java.util.List;
 @Transactional
 public class PilotService { // conver to the interface
     @Autowired
-    private PilotRepo pilotRepo;
+    private PilotRepository pilotRepository;
     @Autowired
     private ModelMapper modelMapper;
 
     public PilotDTO savePilot(PilotDTO pilotDTO){
-        pilotRepo.save(modelMapper.map(pilotDTO, Pilot.class));
+        pilotRepository.save(modelMapper.map(pilotDTO, Pilot.class));
         return pilotDTO;
     }
     public List<PilotDTO> getAllPilots(){
-        List<Pilot> pilotList = pilotRepo.findAll();
+        List<Pilot> pilotList = pilotRepository.findAll();
         return modelMapper.map(pilotList, new TypeToken<List<PilotDTO>>(){}.getType());
     }
     public PilotDTO updatePilot(PilotDTO pilotDTO){
-        pilotRepo.save(modelMapper.map(pilotDTO, Pilot.class));
+        pilotRepository.save(modelMapper.map(pilotDTO, Pilot.class));
         return pilotDTO;
     }
     public boolean deletePilot(int pilotId){
-        if(pilotRepo.existsById(pilotId)){
-            pilotRepo.deleteById(pilotId);
+        if(pilotRepository.existsById(pilotId)){
+            pilotRepository.deleteById(pilotId);
             return true;
         }
         return false;
@@ -41,7 +41,7 @@ public class PilotService { // conver to the interface
     //user id > user details
     // select * from user where id = 2
     public PilotDTO getPilotById(String pilotID){
-        Pilot pilot = pilotRepo.getPilotByPilotID(pilotID);
+        Pilot pilot = pilotRepository.getPilotByPilotID(pilotID);
         return modelMapper.map(pilot, PilotDTO.class);
     }
 
