1cdca06aed10cc801d680f002465569cbe75f381
Chirath Kavindu <95146759+Chira2000@users.noreply.github.com>
2024-09-04 21:09:25 +0530

    Delete src/main directory

--- src/main/java/com/airportmanagementsystem/admin_service/exception/AdminNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.airportmanagementsystem.admin_service.exception;
-
-public class AdminNotFoundException extends RuntimeException{
-    public AdminNotFoundException(String message) {
-        super(message);
-    }
-}
