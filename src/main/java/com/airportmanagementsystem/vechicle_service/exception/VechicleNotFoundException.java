5ea52af0fed347f0d0a66aec61f7e9ebda5558a7
Chirath Kavindu <95146759+Chira2000@users.noreply.github.com>
2024-09-07 00:27:43 +0530

    Delete src directory

--- src/main/java/com/airportmanagementsystem/vechicle_service/exception/VechicleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.airportmanagementsystem.vechicle_service.exception;
-
-public class VechicleNotFoundException extends RuntimeException{
-    public VechicleNotFoundException(String message) {
-        super(message);
-    }
-}
